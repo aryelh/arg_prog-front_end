f15757eaf7789e94f2309c9e9cf3f8ec6d03d5df
aryelh <aryelh@gmail.com>
2023-05-01 23:20:27 -0300

    Se termino de ordenar la vista del resto de los componestes con css, se implementó la dinamicidad en home, queda pendiente el resto, se creó el archivo data.json

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  myData: String[] = [
+    'Sandy Ariel Cruz.'
+  ];
+  aboutMe: String[] = [
+    'Soy programador Web Full Stack que se especializa en crear (y ocacionalmente diseñar) experiencias digitales excepcionales. Actuelmente, me centro en la creación de productos accesibles.'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
